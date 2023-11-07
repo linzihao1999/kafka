23f39c72ed46bfd436d6292e9182a24b56545a4a
Yanming Zhou <zhouyanming@gmail.com>
2023-11-07 17:01:54 +0800

    MINOR: Avoid unnecessary Map lookup in RoundRobinPartitioner (#14613)
    Avoid calling cluster.partitionsForTopic(topic) if cluster.availablePartitionsForTopic(topic) is not empty
    
    Reviewers: Divij Vaidya <diviv@amazon.com>, hudeqi <1217150961@qq.com>

--- core/src/test/scala/unit/kafka/server/ServerStartupTest.scala
+++ core/src/test/scala/unit/kafka/server/ServerStartupTest.scala
@@ -6,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,22 +17,37 @@
 
 package kafka.server
 
-import kafka.utils.TestUtils
+import kafka.integration.KafkaServerTestHarness
+import kafka.utils.TestUtils.createAdminClient
+import kafka.utils.{TestInfoUtils, TestUtils}
 import org.apache.kafka.common.config.ConfigException
+import org.apache.kafka.common.network.ListenerName
+import org.apache.kafka.common.security.auth.SecurityProtocol
 import org.apache.kafka.metadata.BrokerState
 import org.apache.kafka.server.log.remote.storage.{NoOpRemoteLogMetadataManager, NoOpRemoteStorageManager, RemoteLogManagerConfig}
 import org.apache.zookeeper.KeeperException.NodeExistsException
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.{AfterEach, Test}
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
-class ServerStartupTest extends QuorumTestHarness {
+import java.util.Properties
 
-  private var server: KafkaServer = _
+class ServerStartupTest extends KafkaServerTestHarness {
+  val overridingProps = new Properties()
+
+  var broker: KafkaBroker = _
+
+  override def generateConfigs: collection.Seq[KafkaConfig] = {
+    overridingProps.put(KafkaConfig.BrokerIdProp, 0)
+    TestUtils.createBrokerConfigs(1, zkConnectOrNull).map(KafkaConfig.fromProps(_, overridingProps))
+  }
+
 
   @AfterEach
   override def tearDown(): Unit = {
-    if (server != null)
-      TestUtils.shutdownServers(Seq(server))
+    if (broker != null)
+      broker.shutdown()
     super.tearDown()
   }
 
@@ -43,93 +58,89 @@ class ServerStartupTest extends QuorumTestHarness {
     val props = TestUtils.createBrokerConfig(brokerId, zkConnect)
     val zooKeeperConnect = props.get("zookeeper.connect")
     props.put("zookeeper.connect", zooKeeperConnect.toString + zookeeperChroot)
-    server = TestUtils.createServer(KafkaConfig.fromProps(props))
-
+    val broker = createBroker(KafkaConfig.fromProps(props))
     val pathExists = zkClient.pathExists(zookeeperChroot)
     assertTrue(pathExists)
+    broker.shutdown()
   }
 
-  @Test
-  def testConflictBrokerStartupWithSamePort(): Unit = {
-    // Create and start first broker
-    val brokerId1 = 0
-    val props1 = TestUtils.createBrokerConfig(brokerId1, zkConnect)
-    server = TestUtils.createServer(KafkaConfig.fromProps(props1))
-    val port = TestUtils.boundPort(server)
+  @ParameterizedTest(name = TestInfoUtils.TestWithParameterizedQuorumName)
+  @ValueSource(strings = Array("zk", "kraft"))
+  def testConflictBrokerStartupWithSamePort(quorum: String): Unit = {
+    val port = TestUtils.boundPort(brokers.head)
 
     // Create a second broker with same port
     val brokerId2 = 1
-    val props2 = TestUtils.createBrokerConfig(brokerId2, zkConnect, port = port)
-    assertThrows(classOf[IllegalArgumentException], () => TestUtils.createServer(KafkaConfig.fromProps(props2)))
+    val props2 = TestUtils.createBrokerConfig(brokerId2, zkConnectOrNull, port = port)
+    if (isKRaftTest()) {
+      assertThrows(classOf[RuntimeException], () => createBroker(KafkaConfig.fromProps(props2)))
+    } else {
+      assertThrows(classOf[IllegalArgumentException], () => createBroker(KafkaConfig.fromProps(props2)))
+    }
   }
 
-  @Test
-  def testRemoteStorageEnabled(): Unit = {
+  @ParameterizedTest(name = TestInfoUtils.TestWithParameterizedQuorumName)
+  @ValueSource(strings = Array("zk", "kraft"))
+  def testRemoteStorageEnabled(quorum: String): Unit = {
     // Create and start first broker
-    val brokerId1 = 0
-    val props1 = TestUtils.createBrokerConfig(brokerId1, zkConnect)
+    val brokerId1 = 1
+    val props1 = TestUtils.createBrokerConfig(brokerId1, zkConnectOrNull)
     props1.put(RemoteLogManagerConfig.REMOTE_LOG_STORAGE_SYSTEM_ENABLE_PROP, "true")
     props1.put(RemoteLogManagerConfig.REMOTE_STORAGE_MANAGER_CLASS_NAME_PROP, classOf[NoOpRemoteStorageManager].getName)
     props1.put(RemoteLogManagerConfig.REMOTE_LOG_METADATA_MANAGER_CLASS_NAME_PROP, classOf[NoOpRemoteLogMetadataManager].getName)
     props1.put(RemoteLogManagerConfig.REMOTE_LOG_METADATA_MANAGER_LISTENER_NAME_PROP, "badListenerName")
-    assertThrows(classOf[ConfigException], () => TestUtils.createServer(KafkaConfig.fromProps(props1)))
+    assertThrows(classOf[ConfigException], () => createBroker(KafkaConfig.fromProps(props1)))
     // should not throw exception after adding a correct value for "remote.log.metadata.manager.listener.name"
     props1.put(RemoteLogManagerConfig.REMOTE_LOG_METADATA_MANAGER_LISTENER_NAME_PROP, "PLAINTEXT")
-    server = TestUtils.createServer(KafkaConfig.fromProps(props1))
+    createBroker(KafkaConfig.fromProps(props1))
   }
 
-  @Test
-  def testRemoteStorageEnabledWithoutSettingListener(): Unit = {
+  @ParameterizedTest(name = TestInfoUtils.TestWithParameterizedQuorumName)
+  @ValueSource(strings = Array("zk", "kraft"))
+  def testRemoteStorageEnabledWithoutSettingListener(quorum: String): Unit = {
     // Create and start first broker
-    val brokerId1 = 0
-    val props1 = TestUtils.createBrokerConfig(brokerId1, zkConnect)
+    val brokerId1 = 1
+    val props1 = TestUtils.createBrokerConfig(brokerId1, zkConnectOrNull)
     props1.put(RemoteLogManagerConfig.REMOTE_LOG_STORAGE_SYSTEM_ENABLE_PROP, "true")
     props1.put(RemoteLogManagerConfig.REMOTE_STORAGE_MANAGER_CLASS_NAME_PROP, classOf[NoOpRemoteStorageManager].getName)
     props1.put(RemoteLogManagerConfig.REMOTE_LOG_METADATA_MANAGER_CLASS_NAME_PROP, classOf[NoOpRemoteLogMetadataManager].getName)
     // should not throw exception if "remote.log.metadata.manager.listener.name" is unconfigured
-    server = TestUtils.createServer(KafkaConfig.fromProps(props1))
+    broker = createBroker(KafkaConfig.fromProps(props1))
   }
 
-  @Test
-  def testConflictBrokerRegistration(): Unit = {
+  @ParameterizedTest(name = TestInfoUtils.TestWithParameterizedQuorumName)
+  @ValueSource(strings = Array("zk", "kraft"))
+  def testConflictBrokerRegistration(quorum: String): Unit = {
     // Try starting a broker with the a conflicting broker id.
     // This shouldn't affect the existing broker registration.
 
     val brokerId = 0
-    val props1 = TestUtils.createBrokerConfig(brokerId, zkConnect)
-    server = TestUtils.createServer(KafkaConfig.fromProps(props1))
-    val brokerRegistration = zkClient.getBroker(brokerId).getOrElse(fail("broker doesn't exists"))
-
-    val props2 = TestUtils.createBrokerConfig(brokerId, zkConnect)
-    assertThrows(classOf[NodeExistsException], () => TestUtils.createServer(KafkaConfig.fromProps(props2)))
+    val admin = createAdminClient(brokers, ListenerName.forSecurityProtocol(SecurityProtocol.PLAINTEXT))
+//    val brokerRegistration = zkClient.getBroker(brokerId).getOrElse(fail("broker doesn't exists"))
+    //0 : (EndPoint(localhost,56032,ListenerName(PLAINTEXT),PLAINTEXT)) : null : Features{}
+    val brokerRegistration = admin.describeCluster().nodes().get()
+    //    System.out.println(brokerRegistration)
+    val props2 = TestUtils.createBrokerConfig(brokerId, zkConnectOrNull)
+    assertThrows(classOf[NodeExistsException], () => createBroker(KafkaConfig.fromProps(props2)))
 
     // broker registration shouldn't change
     assertEquals(brokerRegistration, zkClient.getBroker(brokerId).getOrElse(fail("broker doesn't exists")))
   }
 
-  @Test
-  def testBrokerSelfAware(): Unit = {
-    val brokerId = 0
-    val props = TestUtils.createBrokerConfig(brokerId, zkConnect)
-    server = TestUtils.createServer(KafkaConfig.fromProps(props))
-
-    TestUtils.waitUntilTrue(() => server.metadataCache.getAliveBrokers().nonEmpty, "Wait for cache to update")
-    assertEquals(1, server.metadataCache.getAliveBrokers().size)
-    assertEquals(brokerId, server.metadataCache.getAliveBrokers().head.id)
+  @ParameterizedTest(name = TestInfoUtils.TestWithParameterizedQuorumName)
+  @ValueSource(strings = Array("zk", "kraft"))
+  def testBrokerSelfAware(quorum: String): Unit = {
+    TestUtils.waitUntilTrue(() => brokers.head.metadataCache.getAliveBrokers().nonEmpty, "Wait for cache to update")
+    assertEquals(1, brokers.head.metadataCache.getAliveBrokers().size)
+    assertEquals(0, brokers.head.metadataCache.getAliveBrokers().head.id)
   }
 
-  @Test
-  def testBrokerStateRunningAfterZK(): Unit = {
-    val brokerId = 0
-
-    val props = TestUtils.createBrokerConfig(brokerId, zkConnect)
-    server = new KafkaServer(KafkaConfig.fromProps(props))
-
-    server.startup()
-    TestUtils.waitUntilTrue(() => server.brokerState == BrokerState.RUNNING,
+  @ParameterizedTest(name = TestInfoUtils.TestWithParameterizedQuorumName)
+  @ValueSource(strings = Array("zk", "kraft"))
+  def testBrokerStateRunningAfterZK(quorum: String): Unit = {
+    TestUtils.waitUntilTrue(() => brokers.head.brokerState == BrokerState.RUNNING,
       "waiting for the broker state to become RUNNING")
-    val brokers = zkClient.getAllBrokersInCluster
     assertEquals(1, brokers.size)
-    assertEquals(brokerId, brokers.head.id)
+    assertEquals(0, brokers.head.config.brokerId)
   }
 }
